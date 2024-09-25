e759548879cb71e1065fc2ed58cfab4509826144
helarye <60302853+helarye@users.noreply.github.com>
2024-09-25 20:02:17 +0300

    Initial commit

---
+++ GitDrill/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GitDrill;
+
+
+Console.WriteLine("Hello, World!");
+Student[] kita = new Student[3];
+kita[0] = new Student( "arye", "arye desc");
+kita[1] = new Student();
+kita[2] = new Student();
+kita[1].Description = "desc1";
+kita[2].Description = "desc2";
+kita[1].Name = "Ofek";
+kita[2].Name = "Peleg";
+
+ConsoleColor[] consCol = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+int colorMaxIndx = consCol.Length;
+int currColIndx = 0;
+
+foreach (Student s in kita)
+{
+    Random random = new Random();
+    currColIndx =random.Next(colorMaxIndx);
+    Console.ForegroundColor = consCol[currColIndx];
+    s.PrintStdDit();
+    //comment 4 ver2
+    
+}
+kita[1].Stam();
+//master changed
+
+
---
+++ GitDrill/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDrill
+{
+    internal class Student
+    {
+        static int nextID = 0;
+        int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Student() 
+        {
+            Id=nextID++;
+            Name = "NameTotav";
+            Description = "DescTotav";
+        }
+        public Student(string name, string description)
+        {
+            Id = nextID++;
+            Name = name;
+            Description = description;
+        }
+        public void PrintStdDit()
+        {
+            
+            Console.WriteLine(this.Id +". "+this.Name+" "+this.Description);
+            //stam1
+        }
+        public void Stam()
+        {
+            Console.WriteLine("in commit2 version");
+            Console.WriteLine("sura B");
+            //stam
+        }
+    }
+}
